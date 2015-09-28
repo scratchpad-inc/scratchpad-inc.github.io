8d4b66dd0ec0b232847f5bbc08c95d19fe958e0d
Team ScratchpadInc <team@scratchpad-inc.com>
2015-09-28 05:58:50 +0300

    Initial

---
+++ site.hs
@@ -0,0 +1,43 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "assets/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "assets/css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Scratchpad Inc. – CryptoNote Blockchain Services" `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
